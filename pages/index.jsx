e8bccbd4ff8f1ca6521159dfef160a02947585f3
Ankit Sharma <ankit.sharma@contentful.com>
2026-04-13 13:09:02 +0200

    Add UK hoodie personalization

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,25 +1,23 @@
 import _ from "lodash";
 import ProductSection from "../components/ProductSection";
 import { getEntriesByContentType } from "../lib/helpers";
+import { Experience } from "@ninetailed/experience.js-next";
+import { ExperienceMapper } from "@ninetailed/experience.js-utils-contentful";
 
-export default function Home(props) {
-  const page = _.get(props, "page");
-  const sections = _.get(page, "fields.sections"); // this field is an array of page sections
+function LandingPage(page) {
+  const sections = _.get(page, "fields.sections");
   const headline = _.get(page, "fields.headline");
-
-  console.log(page); // you can view this object in the console
-
 
   return (
     <>
-      {/* {JSON.stringify(page)} */}
       <h1 className="font-bold text-2xl mb-4 text-center">{headline}</h1>
       <div className="flex flex-col space-y-4">
         {Array.isArray(sections)
-          ? sections.map((section, sectionIndex) => {
+          ? sections.map((section) => {
               const contentType = _.get(section, "sys.contentType.sys.id");
               const sectionId = _.get(section, "sys.id");
               const fields = _.get(section, "fields");
+
               if (contentType === "productSection") {
                 return (
                   <ProductSection
@@ -30,27 +28,46 @@ export default function Home(props) {
                 );
               }
 
-              if (contentType === "product") {
-                return <>{contentType}</>;
-              }
-
-              return "";
+              return null;
             })
-          : ""}
+          : null}
       </div>
     </>
   );
 }
 
+export default function Home({ page }) {
+  const experiences = (page?.fields?.nt_experiences || [])
+    .filter(ExperienceMapper.isExperienceEntry)
+    .map(ExperienceMapper.mapExperience);
+
+  return (
+    <Experience
+      {...page}
+      id={page.sys.id}
+      component={LandingPage}
+      experiences={experiences}
+    />
+  );
+}
+
+
 export async function getStaticProps() {
   const pageEntries = await getEntriesByContentType("landingPage", "home-page");
-  let homepageEntry = _.get(pageEntries, "items[0]");
+
+  const safeEntries = JSON.parse(
+    JSON.stringify(pageEntries, (key, value) => {
+      if (key === "page") return undefined;
+      return value;
+    })
+  );
+
+  const homepageEntry = _.get(safeEntries, "items[0]", {});
 
   return {
     props: {
-      page: homepageEntry ? homepageEntry : {},
+      page: homepageEntry,
     },
-    revalidate: 30, // re-fetch at most once every 30 seconds
-
+    revalidate: 30,
   };
 }
