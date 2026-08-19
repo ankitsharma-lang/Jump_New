992968cf7cba841ed6b158b9016d1fdc89db7865
Ankit Sharma <ankit.sharma@contentful.com>
2026-08-19 17:49:51 +0200

    fix: bundle optimization ESM dependencies

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@ const siteLocales = require("./config/locales")
 
 module.exports = {
     reactStrictMode: true,
+    experimental: {
+        esmExternals: "loose",
+    },
+    transpilePackages: [
+        "@contentful/optimization-api-client",
+        "@contentful/optimization-api-schemas",
+        "@contentful/optimization-core",
+        "@contentful/optimization-nextjs",
+        "@contentful/optimization-node",
+        "@contentful/optimization-react-web",
+        "@contentful/optimization-web",
+        "zod",
+    ],
     i18n: {
         locales: siteLocales.locales,
         defaultLocale: siteLocales.defaultLocale,
